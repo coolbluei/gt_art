eb8fdc7dc9d4a49cbe4856c3eaf8a567788dc59a
David Smallwood <drsmallwood@gmail.com>
2023-11-15 15:52:44 -0500

    config export.  new module better exposed filters.  work on cbi hero banner.

--- web/modules/custom/cbi_art_hero/src/Plugin/Block/HeroBlock.php
+++ web/modules/custom/cbi_art_hero/src/Plugin/Block/HeroBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\cbi_art_hero\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\media\Entity\Media;
 use Drupal\node\NodeInterface;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\safe_field_getter\SafeFieldGetter;
@@ -27,9 +28,11 @@ class HeroBlock extends BlockBase
 
 		$node = \Drupal::routeMatch()->getParameter('node');
 		if ($node instanceof NodeInterface) {
-      $output['data'] = [
-        'title' => $node->getTitle(),
-        'abstract' => SafeFieldGetter::firstSimple($node, 'field_teaser', 'no teaser'),
+      $output = [
+        '#image' => $this->getRenderedMedia($node),
+        '#title' => $node->getTitle(),
+        '#abstract' => SafeFieldGetter::firstSimple($node, 'field_teaser_text', 'no teaser'),
+        '#genre' => $this->getListOfGenre($node),
       ];
 
       $output['#theme'] = 'hero_block';
@@ -38,4 +41,27 @@ class HeroBlock extends BlockBase
 		}
 		return $output;
 	}
+
+  protected function getRenderedMedia(NodeInterface $node): array {
+    $media = SafeFieldGetter::firstReference($node, 'field_media');
+    if (!($media instanceof Media)) {
+      // Always return an array.
+      return [];
+    }
+    $builder = \Drupal::entityTypeManager()->getViewBuilder('media');
+    return $builder->view($media, 'default');
+  }
+
+  protected function getListOfGenre(NodeInterface $node): array{
+    $genres = SafeFieldGetter::allReferences($node, 'field_categories', NULL);
+    if (empty($genres)) {
+      return [];
+    }
+    $results = [];
+    foreach ($genres as $genre) {
+      // Build a new item. @todo
+    }
+    return $results;
+  }
+
 }
